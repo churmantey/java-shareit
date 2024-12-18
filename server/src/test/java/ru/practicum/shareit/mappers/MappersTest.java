a8bad853a2eadba889d181e70c7eba35a347cf8e
Dan Churmanteev <churmantey@yandex.ru>
2024-12-18 13:58:24 +0300

    added comment DTO mapper tests

--- server/src/test/java/ru/practicum/shareit/mappers/MappersTest.java
+++ server/src/test/java/ru/practicum/shareit/mappers/MappersTest.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.practicum.shareit.booking.dto.BookingMapper;
+import ru.practicum.shareit.item.Comment;
+import ru.practicum.shareit.item.Item;
+import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.CommentMapper;
 import ru.practicum.shareit.item.dto.ItemMapper;
 import ru.practicum.shareit.user.User;
@@ -16,6 +19,7 @@ import java.util.List;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
@@ -27,7 +31,7 @@ public class MappersTest {
     private final CommentMapper commentMapper;
 
     @Test
-    public void userToDtoTest() {
+    public void userMapperTests() {
         User user = new User();
         user.setId(1L);
         user.setName("Ivan");
@@ -72,6 +76,42 @@ public class MappersTest {
         assertThat(userList.get(0).getName(), equalTo(userDto1.getName()));
         assertThat(userList.get(1).getId(), equalTo(userDto2.getId()));
         assertThat(userList.get(1).getName(), equalTo(userDto2.getName()));
+
+    }
+
+    @Test
+    public void commentMapperTests() {
+
+        User user = new User();
+        user.setName("Роман");
+        user.setEmail("yandex@mail.com");
+
+        Item item = new Item();
+        item.setName("item");
+        item.setDescription("useful item");
+
+        Comment comment = new Comment();
+        comment.setId(1L);
+        comment.setItem(item);
+        comment.setAuthor(user);
+        comment.setText("some comment");
+
+        CommentDto commentDto = commentMapper.mapToCommentDto(comment);
+        assertEquals(comment.getText(), commentDto.getText());
+        assertEquals(comment.getAuthor().getName(), commentDto.getAuthorName());
+
+        Comment comment2 = new Comment();
+        comment2.setId(2L);
+        comment2.setAuthor(user);
+        comment2.setText("comment #2");
+
+        List<CommentDto> commentDtoList = commentMapper.mapToCommentDtoList(Arrays.asList(comment, comment2));
+
+        assertThat(commentDtoList.size(), equalTo(2));
+        assertThat(commentDtoList.get(0).getId(), equalTo(comment.getId()));
+        assertThat(commentDtoList.get(0).getText(), equalTo(comment.getText()));
+        assertThat(commentDtoList.get(1).getId(), equalTo(comment2.getId()));
+        assertThat(commentDtoList.get(1).getText(), equalTo(comment2.getText()));
 
     }
 
